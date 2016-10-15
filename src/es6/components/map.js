76c30e453b42cdd0a86557f6a4cbc34213513d86
adriennemcd <adrienne.mcd.21@gmail.com>
2016-10-15 14:32:07 -0400

    add sidebar results styling and legend, format dates for results and marker popups

--- src/es6/components/map.js
+++ src/es6/components/map.js
@@ -58,6 +58,28 @@ function buildChart(){
     + cleanDate((weekAgo.getMonth() + 1)) + '-' 
     + cleanDate((weekAgo.getDate()));
 
+    // Function for request result date formatting
+    function formatRequestDate(string) {
+        let months = new Array(12);
+        months[0] = "Jan";
+        months[1] = "Feb";
+        months[2] = "Mar";
+        months[3] = "Apr";
+        months[4] = "May";
+        months[5] = "Jun";
+        months[6] = "Jul";
+        months[7] = "Aug";
+        months[8] = "Sep";
+        months[9] = "Oct";
+        months[10] = "Nov";
+        months[11] = "Dec";
+
+        let date = new Date(string);
+        let month_val = date.getMonth();
+        let formatted_date = months[month_val] + ' ' + date.getDate() + ', ' + date.getFullYear();
+        return formatted_date;
+    }
+    
 
     //Hello! I've set up an api.js module that'll get requests (for now only by ID but in the future it'll hopefully be able to do more)
     //I've also set up the search-form.js as it's own module which calls the api.js module.
@@ -73,18 +95,28 @@ function buildChart(){
             if ( this.lat && this.lon ) {
                 let lat = Number(this.lat);
                 let lon = Number(this.lon);
-                let options = "<p><b>Case ID</b><br>" + this.service_request_id + "<br>";
-                    options += "<b>Status</b><br>" + this.status + "<br>";
-                    options += "<b>Date Requested</b><br>" + this.requested_datetime + '<br>';
-                    options += "<b>Expected Resolution Date</b><br>" + this.expected_datetime + '<br>';
-                    options += "<b>Address</b><br>" + this.address + "<br></p>";
-                let typeInfo = "<h2 class='sidebar__title sidebar__title--sub'>On the Map</h2>";
-                    typeInfo += "<p>Type of Request<br>" + this.service_name + '</p>';
-                    typeInfo += "<p>Agency Responsible<br>" + this.agency_responsible + "</p>";
-                    typeInfo += "<p>The requests shown have been opened within the last week</p>"
-                // let sidebarOptions = "";
+                let requested_date = formatRequestDate(this.requested_datetime);
+                let expected_date = formatRequestDate(this.expected_datetime);
+                let options = "<p class='search-results__item'><span class='search-results__item--key'>Case ID</span>" + this.service_request_id + "</p>";
+                    options += "<p class='search-results__item'><span class='search-results__item--key'>Status</span>" + this.status + "</p>";
+                    options += "<p class='search-results__item'><span class='search-results__item--key'>Date Requested</span>" + requested_date + "</p>";
+                    options += "<p class='search-results__item'><span class='search-results__item--key'>Expected Resolution Date</span>" + expected_date + "</p>";
+                    options += "<p class='search-results__item'><span class='search-results__item--key'>Request Type</span>" + this.service_name + "</p>";
+                    options += "<p class='search-results__item'><span class='search-results__item--key'>Agency Responsible</span>" + this.agency_responsible + "</p>";
+                    options += "<p class='search-results__item'><span class='search-results__item--key'>Address</span>" + this.address + "</p>";
+
+                let popUp = "<p class='search-results__item'><span class='search-results__item--key'>Case ID</span>" + this.service_request_id + "</p>";
+                    popUp += "<p class='search-results__item'><span class='search-results__item--key'>Status</span>" + this.status + "</p>";
+                    popUp += "<p class='search-results__item'><span class='search-results__item--key'>Date Requested</span>" + requested_date + "</p>";
+                    popUp += "<p class='search-results__item'><span class='search-results__item--key'>Expected Resolution Date</span>" + expected_date + "</p>";
+                    popUp += "<p class='search-results__item'><span class='search-results__item--key'>Address</span>" + this.address + "</p>";
+
+                let typeInfo = "<h3 class='search-results__title'>On the Map</h3>";
+                    typeInfo += "<p class='search-results__legend-item'>Your search request</p>";
+                    typeInfo += "<p class='search-results__legend-item search-results__legend-item--related'>Other " + this.service_name + " requests opened within the last seven days</p>";
+
                 new L.marker([lat, lon], {icon: yourRequestIcon})
-                .addTo( markersLayer ).bindPopup(options);
+                .addTo( markersLayer ).bindPopup(popUp);
                 
                 $('.js-request-details').empty().append(options);
                 $('.js-request-type').empty().append(typeInfo);
@@ -113,14 +145,19 @@ function buildChart(){
                     if ( this.service_request_id == originalRequestID ) {
                         //don't plot if this is the one we already have
                         return;
-                    }
+                    } 
                     var lat = Number(this.lat);
                     var lon = Number(this.lon);
-                    var options = "<p><b>Case ID</b><br>" + this.service_request_id + "<br>";
-                        options += "<b>Address</b><br>" + this.address + "<br>";
-                        options += "<b>Status</b><br>" + this.status + "<br></p>";
+                    let requested_date = formatRequestDate(this.requested_datetime);
+                    let expected_date = formatRequestDate(this.expected_datetime);
+                    let popUp = "<p class='search-results__item'><span class='search-results__item--key'>Case ID</span>" + this.service_request_id + "</p>";
+                        popUp += "<p class='search-results__item'><span class='search-results__item--key'>Status</span>" + this.status + "</p>";
+                        popUp += "<p class='search-results__item'><span class='search-results__item--key'>Date Requested</span>" + requested_date + "</p>";
+                        popUp += "<p class='search-results__item'><span class='search-results__item--key'>Expected Resolution Date</span>" + expected_date + "</p>";
+                        popUp += "<p class='search-results__item'><span class='search-results__item--key'>Address</span>" + this.address + "</p>";
+                    
                     new L.marker([lat, lon], {icon: otherRequestsIcon})
-                    .addTo( markersLayer ).bindPopup(options);
+                    .addTo( markersLayer ).bindPopup(popUp);
                 } else {
                     console.log("incomplete geographic info");
                 }
