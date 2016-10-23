968e112af61c21b89aee9ec2660f1eb98a802a23
Iain J McCallum <ijmccallum@hotmail.co.uk>
2016-10-22 20:29:29 -0400

    mobile menu slide in

--- dist/js/common.bundle.js
+++ dist/js/common.bundle.js
@@ -41988,8 +41988,22 @@ module.exports = {
 	init(){
 		$('.js-mobile-menu-button').on('click', function(){
 			console.log('toggle the mobile menu!');
+			var menuName = $(this).data('mobile-menu-name');//so we only slide this specific mobile menu
 
-			$('.js-mobile-menu-slide').toggleSlide();
+			//now look for any sidebar mobile slider thing with the same name
+			$('.js-mobile-menu-slide').each(function(){
+				var $thisSidebar = $(this);
+				if ($thisSidebar.data('mobile-menu-name') == menuName) {
+
+					$thisSidebar.toggleClass('open');
+
+				}
+			});
+
+		});
+
+		$(window).on('resize', function() {
+
 		});
 	}
 }
